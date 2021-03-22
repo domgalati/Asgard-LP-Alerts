5a04ce0f9d179ae838ad218bcaf62d08f8e21f27
andihow <andihow@gmail.com>
2021-03-22 04:40:26 -0400

    added start/stop and a few other things

--- index.js
+++ index.js
@@ -11,7 +11,13 @@ var ping = async() => {
 ping()
 
 /*Pairs to check against RUNE*/
-var quoteccy = ["bitcoin-cash","bitcoin","ethereum","tether","litecoin","binancecoin","binance-usd"]
+var quoteccy = [{"geckid":"bitcoin-cash","shorthand":"bch"},
+	{"geckid":"bitcoin","shorthand":"btc"},
+	{"geckid":"ethereum","shorthand":"eth"},
+	{"geckid":"tether","shorthand":"usdt"},
+	{"geckid":"litecoin","shorthand":"ltc"},
+	{"geckid":"binancecoin","shorthand":"bnb"},
+	{"geckid":"binance-usd","shorthand":"busd"}]
 
 /*Fetch rune price increase percentage and thumnail icon*/
 var fetchRuneData = async () => {
@@ -46,6 +52,7 @@ var fetchCoinData = async(cointocheck) => {
 DiscordClient.login("ODIxOTU0NjEwODMwMTE0ODI5.YFLO4g.ZvgeF-SfTXbqagopNRbYkJ7xWFM");
 DiscordClient.once('ready', () => {
 	console.log('Bot Logged Into Discord Channel');
+	status = false
 });
 var timer
 DiscordClient.on('message', async (message) => {
@@ -60,59 +67,85 @@ DiscordClient.on('message', async (message) => {
 	}
 
 	if (message.content.startsWith (prefix + "start")){
-		mostrecentquotepercent = {}
-		var getPercentSpreads = async() => {
-			let runepercent = await fetchRuneData();
-			for (i=0; i < quoteccy.length; i++) {
-				let quotepercent = await fetchCoinData(quoteccy[i]);
-				let total = runepercent - quotepercent
-				let difference = quotepercent - runepercent
-				if (runepercent > quotepercent && total >= 1) {				
-					output = new Discord.MessageEmbed()
-						.setThumbnail(cointhumb)
-						.setImage(runethumb)
-						.setTitle('rune.'+quoteccy[i]+' pool alert!')
-						.setAuthor('Asgard LP Update')
-						.setDescription('RUNE is outperforming '+quoteccy[i].toString().toUpperCase()+' by '+total+"%!")
-						.addField("INFO",'RUNE has changed by '+runepercent+'%!', false)
-						.setTimestamp();
-					output.coinid = quoteccy[i]
-				}
-				else if (runepercent < quotepercent && total <= -1) {			
-					output = new Discord.MessageEmbed()
-						.setThumbnail(cointhumb)
-						.setImage(runethumb)
-						.setTitle('rune.'+quoteccy[i]+' pool alert!')
-						.setAuthor('Asgard LP Update')
-						.setDescription(quoteccy[i].toString().toUpperCase()+' is outperforming RUNE by '+difference+"%!")
-						.addField("INFO",quoteccy[i].toString().toUpperCase()+' has changed by '+quotepercent+'%!')
-						.setTimestamp();
-					output.coinid = quoteccy[i]
-				}
-				else {
-					output = false
-				}
-				if (output !== false) {
-					if (mostrecentquotepercent[quoteccy[i]] == quotepercent || mostrecentquotepercent[quoteccy[i]] - quotepercent < 1){
-						console.log("Supressing duplicate message for: " + quoteccy[i])
-						console.log(output)
+		if (status == false){
+			console.log(Date() + " start command issued")
+			message.channel.send(":yellow_circle: Starting engines...")
+			message.channel.send(":green_circle: I am currently monitoring the market for price changes :mag: - use !stop to shut me off")
+
+			mostrecentquotepercent = {}
+			var getPercentSpreads = async() => {
+				let runepercent = await fetchRuneData();
+				for (i=0; i < quoteccy.length; i++) {
+					let quotepercent = await fetchCoinData(quoteccy[i]["geckid"]);
+					let total = runepercent - quotepercent
+					let difference = quotepercent - runepercent
+					if (runepercent > quotepercent && total >= 1) {				
+						output = new Discord.MessageEmbed()
+							.setThumbnail(cointhumb)
+							.setImage(runethumb)
+							.setTitle('RUNE.'+quoteccy[i]["shorthand"].toString().toUpperCase()+' pool alert!')
+							.setAuthor('Asgard LP Update')
+							.setDescription('RUNE is outperforming '+quoteccy[i]["geckid"].toString().toUpperCase()+' by '+total.toString().substring(0,5)+"%")
+							.addField(":notebook_with_decorative_cover: More Info",'RUNE has changed by '+runepercent.toString().substring(0,5)+'%'+'\n'+quoteccy[i]["geckid"].toString().toUpperCase()+' has changed by '+quotepercent.toString().substring(0,5)+'%')
+							.setTimestamp();
+						output.coinid = quoteccy[i]
+					}
+					else if (runepercent < quotepercent && total <= -1) {			
+						output = new Discord.MessageEmbed()
+							.setThumbnail(cointhumb)
+							.setImage(runethumb)
+							.setTitle(quoteccy[i]["shorthand"].toString().toUpperCase()+'.RUNE pool alert!')
+							.setAuthor('Asgard LP Update')
+							.setDescription(quoteccy[i]["geckid"].toString().toUpperCase()+' is outperforming RUNE by '+difference.toString().substring(0,5)+"%")
+							.addField(":notebook_with_decorative_cover: More Info",quoteccy[i]["geckid"].toString().toUpperCase()+' has changed by '+quotepercent.toString().substring(0,5)+'%'+'\n'+'RUNE has changed by '+runepercent.toString().substring(0,5)+'%')
+							.setTimestamp();
+						output.coinid = quoteccy[i]["geckid"]
 					}
 					else {
-						console.log(output)
-						message.channel.send(output);
-						mostrecentquotepercent[quoteccy[i]] = quotepercent
-					}		 
+						output = false
+					}
+					if (output !== false) {
+						if (mostrecentquotepercent[quoteccy[i]["geckid"]] == quotepercent || mostrecentquotepercent[quoteccy[i]["geckid"]] - quotepercent < 1){
+							console.log("Supressing duplicate message for: " + quoteccy[i]["geckid"])
+							console.log(output)
+						}
+						else {
+							console.log(output)
+							message.channel.send(output);
+							mostrecentquotepercent[quoteccy[i]["geckid"]] = quotepercent
+						}		 
+					}
 				}
 			}
+
+			timer = setInterval(function(){
+				getPercentSpreads()
+				console.log("Checking Prices...")
+			}, 10000)
+			status = true
 		}
-		timer = setInterval(function(){
-			getPercentSpreads()
-			console.log("Checking Prices...")
-		}, 10000)
+		else {
+			message.channel.send(":red_circle: I can't start if I'm already running :man_running:")
+		}
 	}
 
 	if (message.content.startsWith (prefix + "stop")) {
-		message.channel.send("Stopping alerts...")
-		clearInterval(timer)
+		if (status == false){
+			message.channel.send(":red_circle: Bringing alert system down ...")
+			clearInterval(timer)
+			mostrecentquotepercent = {}
+			status = false
+		} else {
+			message.channel.send(":red_circle: It isnt running...")
+		}
+	}
+
+	if (message.content.startsWith (prefix + "status") || message.content.startsWith (prefix + "help")) {
+		if (status == false) {
+			message.channel.send("I am not sending alerts right now. Use !start to turn me on!")
+		}
+		if (status == true) {
+			message.channel.send("I am currently monitoring the market for price changes :mag: - use !stop to shut me off")
+		}
 	}
 });
